5503f997144c09085ba00890b5a4d5a1e1b4ac49
Tonyf6 <149448421+Tonyf6@users.noreply.github.com>
2024-06-05 21:12:07 -0700

    updating

--- hook_forms/src/App.tsx
+++ hook_forms/src/App.tsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from 'react';
+import ExpenseList from './components/ExpenseList';
+import './App.css';
 
-const App = () => {
+const App: React.FC = () => {
   return (
-    <div>App</div>
-  )
-}
+    <div className="App">
+      <h1>Expense Tracker</h1>
+      <ExpenseList />
+    </div>
+  );
+};
 
-export default App
+export default App;
